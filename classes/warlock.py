f4021aded976297fda038dc5614df6b95ffadc91
BobMowzie <jnad325@gmail.com>
2022-06-13 21:34:13 -0700

    Fix warlock

--- classes/warlock.py
+++ classes/warlock.py
@@ -2,96 +2,97 @@ from playerBase import PlayerBase
 from abilityBase import AbilityBase
 from effectBase import EffectBase
 
+
 class Warlock(PlayerBase):
-  def __init__(self, user, game):
-    PlayerBase.__init__(self, user, game)
-    self.soulboundEffect
+    def __init__(self, user, game):
+        PlayerBase.__init__(self, user, game)
+        self.soulboundEffect = None
 
-  @classmethod
-  def className(cls):
-    return 'Warlock'
+    @classmethod
+    def className(cls):
+        return 'Warlock'
 
-  @classmethod
-  def classEmoji(cls):
-    return '💀'
-  
-  @classmethod
-  def classDescription(cls):
-    return "Warlocks are a high-risk high-reward offensive class. They sacrifice their own health to deal damage."
+    @classmethod
+    def classEmoji(cls):
+        return '💀'
 
-  @classmethod
-  def ability1(self):
-    return EldrichBlast
+    @classmethod
+    def classDescription(cls):
+        return "Warlocks are a high-risk high-reward offensive class. They sacrifice their own health to deal damage."
 
-  @classmethod
-  def ability2(self):
-    return Soulbind
-    
-  def addDamageSource(self, amount, damager, source=None):
-  	if not instanceof(source, SoulboundEffect):
-  		if self.soulboundEffect:
-  			target = self.soulboundEffect.target
-  			if self.soulboundEffect in target.activeEffects:
-  				target.addDamageSource(amount, self, self.soulboundEffect)
-  	PlayerBase.addDamageSource(self, amount, damager, source)
+    @classmethod
+    def ability1(cls):
+        return EldritchBlast
+
+    @classmethod
+    def ability2(cls):
+        return Soulbind
+
+    def addDamageSource(self, amount, damager, source=None):
+        if not isinstance(source, SoulboundEffect):
+            if self.soulboundEffect:
+                target = self.soulboundEffect.target
+                if self.soulboundEffect in target.activeEffects:
+                    target.addDamageSource(amount, self, self.soulboundEffect)
+            PlayerBase.addDamageSource(self, amount, damager, source)
+
 
 #######################################
 # Abilities
 #######################################
 class EldritchBlast(AbilityBase):
-  def __init__(self, caster: Wizard, targets):
-    AbilityBase.__init__(self, caster, targets)
-  
-  @classmethod
-  def abilityName(cls):
-    return "Eldritch Blast"
-  
-  @classmethod
-  def abilityDescription(cls):
-    return 'Deal 3 damage to a target and 1 damage to yourself.'
+    def __init__(self, caster: Warlock, targets):
+        AbilityBase.__init__(self, caster, targets)
 
-  def damageEffect(self):
-    target = self.targets[0]
-    self.caster.dealDamage(target, 3)
-    self.caster.dealDamage(self.caster, 1)
+    @classmethod
+    def abilityName(cls):
+        return "Eldritch Blast"
 
-  def canUse(self):
-    return len(self.targets) == 1 and self.targets[0] != self.caster
+    @classmethod
+    def abilityDescription(cls):
+        return 'Deal 3 damage to a target and 1 damage to yourself.'
+
+    def damageEffect(self):
+        target = self.targets[0]
+        self.caster.dealDamage(target, 3)
+        self.caster.dealDamage(self.caster, 1)
+
+    def canUse(self):
+        return len(self.targets) == 1 and self.targets[0] != self.caster
+
 
 class Soulbind(AbilityBase):
-  def __init__(self, caster: Wizard, targets):
-    AbilityBase.__init__(self, caster, targets)
+    def __init__(self, caster: Warlock, targets):
+        AbilityBase.__init__(self, caster, targets)
 
-  @classmethod
-  def abilityName(cls):
-    return "Soulbind"
-  
-  @classmethod
-  def abilityDescription(cls):
-    return 'Bind your soul to a target for 3 turns. Whenever you take damage, they take damage too.'
-  
-  def applyEffects(self):
-    self.caster.soulboundEffect = SoulboundEffect(self.caster, target, 3)
-    target = self.targets[0]
-    target.addEffect(self.caster.soulboundEffect)
-    
-  def canUse(self):
-    return len(self.targets) == 1 and self.targets[0] != self.caster
+    @classmethod
+    def abilityName(cls):
+        return "Soulbind"
+
+    @classmethod
+    def abilityDescription(cls):
+        return 'Bind your soul to a target for 3 turns. Whenever you take damage, they take damage too.'
+
+    def applyEffects(self):
+        target = self.targets[0]
+        self.caster.soulboundEffect = SoulboundEffect(self.caster, target, 3)
+        target.addEffect(self.caster.soulboundEffect)
+
+    def canUse(self):
+        return len(self.targets) == 1 and self.targets[0] != self.caster
+
 
 #######################################
 # Effects
 #######################################
 class SoulboundEffect(EffectBase):
-  def __init__(self, caster, target, turnsRemaining):
-    EffectBase.__init__(self, caster, target, turnsRemaining)
+    def __init__(self, caster, target, turnsRemaining):
+        EffectBase.__init__(self, caster, target, turnsRemaining)
 
-  def damageEffect(self):
-    self.caster.dealDamage(self.target, 1)
+    @classmethod
+    def effectName(cls):
+        return "Soulbound"
 
-  @classmethod
-  def effectName(cls):
-    return "Soulbound"
-
-  @classmethod
-  def effectEmoji(self):
-    return '👻'
+    @classmethod
+    def effectEmoji(cls):
+        return '👻'
--- playerBase.py
+++ playerBase.py
@@ -1,188 +1,195 @@
 from abc import ABC, abstractmethod
 import math
 
+
 def toSub(x):
-  normal = "0123456789"
-  sub_s = "₀₁₂₃₄₅₆₇₈₉"
-  res = x.maketrans(''.join(normal), ''.join(sub_s))
-  return x.translate(res)
+    normal = "0123456789"
+    sub_s = "₀₁₂₃₄₅₆₇₈₉"
+    res = x.maketrans(''.join(normal), ''.join(sub_s))
+    return x.translate(res)
+
 
 class PlayerBase(ABC):
-  """A player character class"""
-  def __init__(self, user, game):
-    self.user = user
-    self.game = game
-    self.team = None
-    self.health = 15
-    self.damageTakenThisTurn = 0
-    self.damageTakenLastTurn = 0
-    self.attackMultiplier = 1
-    self.takeDamageMultiplier = 1
-    self.takeDamageReduction = 0
-    self.activeEffects = []
-    self.alive = True
-    self.curingClerics = []
-    self.damageTaken = []    # List of (source player, amount) tuples
-    self.damageDealt = {}    # Dict mapping target player to amount
+    """A player character class"""
 
-  @classmethod
-  @abstractmethod
-  def ability1(self):
-    pass
+    def __init__(self, user, game):
+        self.user = user
+        self.game = game
+        self.team = None
+        self.health = 15
+        self.damageTakenThisTurn = 0
+        self.damageTakenLastTurn = 0
+        self.attackMultiplier = 1
+        self.takeDamageMultiplier = 1
+        self.takeDamageReduction = 0
+        self.activeEffects = []
+        self.alive = True
+        self.curingClerics = []
+        self.damageTaken = []  # List of (source player, amount) tuples
+        self.damageDealt = {}  # Dict mapping target player to amount
 
-  @classmethod
-  @abstractmethod
-  def ability2(self):
-    pass
+    @classmethod
+    @abstractmethod
+    def ability1(cls):
+        pass
 
-  @classmethod
-  def getInfo(self):
-    toPrint = "**" + self.classEmoji() + " " + self.className() + "**\n"
-    toPrint += self.classDescription() + "\n"
-    toPrint += "**Ability 1: " + self.ability1().abilityName() + "\n**"
-    toPrint += self.ability1().abilityDescription() + "\n"
-    toPrint += "**Ability 2: " + self.ability2().abilityName() + "\n**"
-    toPrint += self.ability2().abilityDescription() + "\n"
-    return toPrint
-  
-  def doAbility(self, abilityClass, target):
-    return abilityClass(self, target)
+    @classmethod
+    @abstractmethod
+    def ability2(cls):
+        pass
 
-  def skipTurn(self):
-    pass
+    @classmethod
+    def getInfo(cls):
+        toPrint = "**" + cls.classEmoji() + " " + cls.className() + "**\n"
+        toPrint += cls.classDescription() + "\n"
+        toPrint += "**Ability 1: " + cls.ability1().abilityName() + "\n**"
+        toPrint += cls.ability1().abilityDescription() + "\n"
+        toPrint += "**Ability 2: " + cls.ability2().abilityName() + "\n**"
+        toPrint += cls.ability2().abilityDescription() + "\n"
+        return toPrint
 
-  def startTurn(self):
-    self.activeEffects = [effect for effect in self.activeEffects if effect.turnsRemaining > 0]
+    def doAbility(self, abilityClass, target):
+        return abilityClass(self, target)
 
-  def modifyActions(self):
-    pass
+    def skipTurn(self):
+        pass
 
-  def applyEffects(self):
-    if len(self.curingClerics) > 0:
-      for cleric in self.curingClerics:
-        cleric.blessings += len(self.activeEffects)
-      self.activeEffects.clear()
+    def startTurn(self):
+        self.activeEffects = [effect for effect in self.activeEffects if effect.turnsRemaining > 0]
 
-  def preDamagePhase(self):
-    pass
+    def modifyActions(self):
+        pass
 
-  def dealDamage(self, target, amount, source=None):
-    amount = amount * self.attackMultiplier
-    target.addDamageSource(amount, self, source)
-      
-  def addDamageSource(self, amount, damager, source=None):
-  	from classes.paladin import ShieldEffect
-    blockingPaladins = [effect.caster for effect in self.activeEffects if isinstance(effect, ShieldEffect)]
-    if len(blockingPaladins) > 0:
-      blockedAmount = math.floor(amount / 2)
-      for paladin in blockingPaladins:
-        paladin.damageTaken.append((damager, blockedAmount))
-        if amount > paladin.biggestAttack:
-          paladin.biggestAttack = amount
-    else:
-      self.damageTaken.append((damager, amount))
+    def applyEffects(self):
+        if len(self.curingClerics) > 0:
+            for cleric in self.curingClerics:
+                cleric.blessings += len(self.activeEffects)
+            self.activeEffects.clear()
 
-  def modifyDamage(self):
-    self.damageTaken.sort(key=lambda x:x[1])
+    def preDamagePhase(self):
+        pass
 
-    from classes.cleric import DivineBarrierEffect
-    largestBlockedSoFar = 0
-    divineBarriers = [effect for effect in self.activeEffects if isinstance(effect, DivineBarrierEffect) and effect.turnsRemaining > 0]
-    for i in range(len(self.damageTaken)):
-      source = self.damageTaken[i][0]
-      amount = self.damageTaken[i][1]
-      if amount > largestBlockedSoFar:
-        largestBlockedSoFar = amount
-        for dBarrier in divineBarriers:
-          dBarrier.turnsRemaining -= 1
-          divineBarriers = [barrier for barrier in divineBarriers if barrier.turnsRemaining >= 0]
-      if len(divineBarriers) > 0:
-        self.damageTaken[i] = (source, 0)
+    def dealDamage(self, target, amount, source=None):
+        amount = amount * self.attackMultiplier
+        target.addDamageSource(amount, self, source)
 
-  def takeDamage(self):
-    for player, amount in self.damageTaken:
-      amount = max(0, amount - self.takeDamageReduction)
-      amount = math.floor(amount * self.takeDamageMultiplier)
-      self.health -= amount
-      self.damageTakenThisTurn += amount
-      if player.damageDealt.get(self):
-        player.damageDealt[self] += amount
-      else:
-        player.damageDealt[self] = amount
-      if self.health <= 0:
-        self.health = 0
-        self.alive = False
+    def addDamageSource(self, amount, damager, source=None):
+        from classes.paladin import ShieldEffect
+        blockingPaladins = [effect.caster for effect in self.activeEffects if isinstance(effect, ShieldEffect)]
+        if len(blockingPaladins) > 0:
+            blockedAmount = math.floor(amount / 2)
+            for paladin in blockingPaladins:
+                paladin.damageTaken.append((damager, blockedAmount))
+                if amount > paladin.biggestAttack:
+                    paladin.biggestAttack = amount
+        else:
+            self.damageTaken.append((damager, amount))
 
-  def endTurn(self):
-    self.damageTakenLastTurn = self.damageTakenThisTurn
-    self.damageTakenThisTurn = 0
-    self.attackMultiplier = 1
-    self.takeDamageMultiplier = 1
-    self.takeDamageReduction = 0
-    for effect in self.activeEffects:
-      effect.turnsRemaining = max(effect.turnsRemaining - 1, 0)
-    self.curingClerics.clear()
-    self.damageTaken.clear()
-    self.damageDealt.clear()
+    def modifyDamage(self):
+        self.damageTaken.sort(key=lambda x: x[1])
 
-  @classmethod
-  def classDescription(cls):
-    return ''
+        from classes.cleric import DivineBarrierEffect
+        largestBlockedSoFar = 0
+        divineBarriers = [effect for effect in self.activeEffects if
+                          isinstance(effect, DivineBarrierEffect) and effect.turnsRemaining > 0]
+        for i in range(len(self.damageTaken)):
+            source = self.damageTaken[i][0]
+            amount = self.damageTaken[i][1]
+            if amount > largestBlockedSoFar:
+                largestBlockedSoFar = amount
+                for dBarrier in divineBarriers:
+                    dBarrier.turnsRemaining -= 1
+                    divineBarriers = [barrier for barrier in divineBarriers if barrier.turnsRemaining >= 0]
+            if len(divineBarriers) > 0:
+                self.damageTaken[i] = (source, 0)
 
-  @classmethod
-  def className(cls):
-    return ''
+    def takeDamage(self):
+        for player, amount in self.damageTaken:
+            amount = max(0, amount - self.takeDamageReduction)
+            amount = math.floor(amount * self.takeDamageMultiplier)
+            self.health -= amount
+            self.damageTakenThisTurn += amount
+            if player.damageDealt.get(self):
+                player.damageDealt[self] += amount
+            else:
+                player.damageDealt[self] = amount
+            if self.health <= 0:
+                self.health = 0
+                self.alive = False
 
-  @classmethod
-  def classEmoji(cls):
-    return ''
+    def endTurn(self):
+        self.damageTakenLastTurn = self.damageTakenThisTurn
+        self.damageTakenThisTurn = 0
+        self.attackMultiplier = 1
+        self.takeDamageMultiplier = 1
+        self.takeDamageReduction = 0
+        for effect in self.activeEffects:
+            effect.turnsRemaining = max(effect.turnsRemaining - 1, 0)
+        self.curingClerics.clear()
+        self.damageTaken.clear()
+        self.damageDealt.clear()
 
-  def hasEffect(self, effectType):
-    return any(isinstance(effect, effectType) for effect in self.activeEffects)
+    @classmethod
+    def classDescription(cls):
+        return ''
 
-  def removeEffect(self, effectType):
-    self.activeEffects = [effect for effect in self.activeEffects if not isinstance(effect, effectType)]
+    @classmethod
+    def className(cls):
+        return ''
 
-  def addEffect(self, effect):
-    if len(self.curingClerics) > 0:
-      for cleric in self.curingClerics:
-        cleric.blessings += 1
-      return
-    self.activeEffects.append(effect)
+    @classmethod
+    def classEmoji(cls):
+        return ''
 
-  def toString(self):
-    toReturn = self.classEmoji()
-    resourceNumber = self.resourceNumber()
-    if resourceNumber:
-      toReturn += toSub(str(resourceNumber))
-    toReturn += '❤️' + toSub(str(self.health))
-    toReturn += "<@" + str(self.user.id) + "> "
-    for effect in self.activeEffects:
-      toReturn += effect.effectEmoji() + toSub(str(effect.turnsRemaining))
-    return toReturn
+    def hasEffect(self, effectType):
+        return any(isinstance(effect, effectType) for effect in self.activeEffects)
 
-  def resourceNumber(self):
-    return None
+    def removeEffect(self, effectType):
+        self.activeEffects = [effect for effect in self.activeEffects if not isinstance(effect, effectType)]
+
+    def addEffect(self, effect):
+        if len(self.curingClerics) > 0:
+            for cleric in self.curingClerics:
+                cleric.blessings += 1
+            return
+        self.activeEffects.append(effect)
+
+    def toString(self):
+        toReturn = self.classEmoji()
+        resourceNumber = self.resourceNumber()
+        if resourceNumber:
+            toReturn += toSub(str(resourceNumber))
+        toReturn += '❤️' + toSub(str(self.health))
+        toReturn += "<@" + str(self.user.id) + "> "
+        for effect in self.activeEffects:
+            toReturn += effect.effectEmoji() + toSub(str(effect.turnsRemaining))
+        return toReturn
+
+    def resourceNumber(self):
+        return None
+
 
 from classes.barbarian import Barbarian
 from classes.paladin import Paladin
 from classes.wizard import Wizard
 from classes.cleric import Cleric
 from classes.druid import Druid
+from classes.warlock import Warlock
 
 classEmojis = {
-  Barbarian.classEmoji():Barbarian,
-  Paladin.classEmoji():Paladin,
-  Wizard.classEmoji():Wizard,
-  Cleric.classEmoji():Cleric,
-  Druid.classEmoji():Druid
+    Barbarian.classEmoji(): Barbarian,
+    Paladin.classEmoji(): Paladin,
+    Wizard.classEmoji(): Wizard,
+    Cleric.classEmoji(): Cleric,
+    Druid.classEmoji(): Druid,
+    Warlock.classEmoji(): Warlock
 }
 
 classNames = {
-  Barbarian.className():Barbarian,
-  Paladin.className():Paladin,
-  Wizard.className():Wizard,
-  Cleric.className():Cleric,
-  Druid.className():Druid
+    Barbarian.className(): Barbarian,
+    Paladin.className(): Paladin,
+    Wizard.className(): Wizard,
+    Cleric.className(): Cleric,
+    Druid.className(): Druid,
+    Warlock.className(): Warlock
 }
-  
